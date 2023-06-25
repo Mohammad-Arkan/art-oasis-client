8c3719d3bc2d345886735e6ab81e19c2f6c1ef51
Zihadul Islam <zynscriptofficial@gmail.com>
2023-06-25 19:40:29 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,7 @@ import {RouterProvider} from "react-router-dom";
 import router from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <div className="max-w-screen-xl mx-auto">
+    <RouterProvider router={router} />
+  </div>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h2 className="text-3xl">Art Oasis</h2>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
