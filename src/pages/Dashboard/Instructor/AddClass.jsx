b00bf85d9e7d7b97cb4cbfd7977f22e644635e40
Zihadul Islam <zynscriptofficial@gmail.com>
2023-06-30 15:19:24 +0600

    add class data on submit

--- src/pages/Dashboard/Instructor/AddClass.jsx
+++ src/pages/Dashboard/Instructor/AddClass.jsx
@@ -1,7 +1,119 @@
 import React from "react";
+import {useForm} from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
+const img_hosting_token = import.meta.env.VITE_Img_Hosting_Token;
 
 const AddClass = () => {
-  return <div>Add Class</div>;
+  const [axiosSecure] = useAxiosSecure();
+  const {user} = useAuth();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: {errors},
+  } = useForm();
+  const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="w-full">
+      <div className="mx-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="space-y-1 mt-20 lg:mt-0">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold">Class Name*</span>
+            </label>
+            <input
+              type="text"
+              {...register("className", {required: true})}
+              placeholder="Class Name"
+              className="input input-bordered input-accent w-full"
+            />
+          </div>
+
+          <div className="flex gap-3 pt-3">
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text font-semibold">
+                  Instructor Name*
+                </span>
+              </label>
+              <input
+                type="text"
+                defaultValue={user?.displayName}
+                readOnly
+                {...register("instructorName", {required: true})}
+                placeholder="Instructor Name"
+                className="input input-bordered input-accent w-full"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text font-semibold">
+                  Instructor Email*
+                </span>
+              </label>
+              <input
+                type="text"
+                {...register("instructorEmail", {required: true})}
+                defaultValue={user.email}
+                readOnly
+                placeholder="Instructor Email"
+                className="input input-bordered input-accent w-full"
+              />
+            </div>
+          </div>
+          <div className="flex gap-3 pt-3">
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Available Seats*</span>
+              </label>
+              <input
+                type="number"
+                placeholder="Available Seats"
+                {...register("availableSeats", {required: true})}
+                className="input input-bordered input-accent w-full"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text font-semibold">Price*</span>
+              </label>
+              <input
+                type="number"
+                placeholder="Class Price"
+                {...register("price", {required: true})}
+                className="input input-bordered input-accent w-full"
+              />
+            </div>
+          </div>
+          <div className="form-control pb-2">
+            <label className="label">
+              <span className="label-text">Class Image*</span>
+            </label>
+            <input
+              type="file"
+              {...register("image", {required: true})}
+              className="file-input file-input-bordered w-full"
+            />
+          </div>
+          <input
+            className="btn btn-outline btn-sm"
+            type="submit"
+            value="Add Class"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddClass;
